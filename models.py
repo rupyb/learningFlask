9014101b6d6d3a54fd447b9310df3a11f4736806
reuvenbrisman <rupybrisman@yahoo.com>
2018-10-18 00:58:42 +0100

    new

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ db =SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     uid = db.Column(db.Integer, primary_key =True)
-    firstname = db.Column(db.String(100)))
+    firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
     email = db.Column(db.String(120), unique = True)
     pwdhash = db.Column(db.String(54))
