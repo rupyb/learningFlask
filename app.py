9d3d9e2ac45c197b415daab1faabbb701aad1ab6
rupyb <rupybrisman@yahoo.com>
2018-12-10 16:00:46 -0500

    new

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 from flask import Flask
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvjcfvnbjpypnp:b72e0c4572c5e73b5af3c451d2f4cfb69d77f569a11fcb406b5fb470a46ba848@ec2-54-83-197-230.compute-1.amazonaws.com:5432/dpnm7g9f3mtrb'
